d20d4dfa2bfed2decf1d5bde7b4df780385f9207
songnan wu <tcwsn321@outlook.com>
2017-02-05 14:40:47 -0500

    Oliver

--- SuperMarioGame/SuperMarioGame/StateClass/CharacterClass/Mario/Mario.cs
+++ SuperMarioGame/SuperMarioGame/StateClass/CharacterClass/Mario/Mario.cs
@@ -13,7 +13,38 @@ namespace SuperMarioGame.StateClass.CharacterClass.Mario
         {
             state = new SpriteFactories.MarioSpriteFactory(this);
         }
+        public void MarioIdle()
+        {
 
+        }
+        public  void MarioChangeDirection()
+        {
+
+        }
+        public void MarioChangeForm()
+        {
+
+        }
+        public void MarioJump()
+        {
+
+        }
+        public void MarioCrounch()
+        {
+
+        }
+        public void MarioRun()
+        {
+
+        }
+        public void MarioDraw()
+        {
+
+        }
+        public void MarioUpdate()
+        {
+
+        }
 
     }
 }
