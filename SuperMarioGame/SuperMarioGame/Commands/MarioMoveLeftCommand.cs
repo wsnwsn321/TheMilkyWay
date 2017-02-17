bfe8f637fa5537fe2d50db4d7e74199834cc7d3d
George <yang.2766@osu.edu>
2017-02-17 16:45:56 -0500

    02/17/2017 George

---
+++ SuperMarioGame/SuperMarioGame/Commands/MarioMoveLeftCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace SuperMarioGame.Commands.Sprint3
+{
+    class MarioMoveLeftCommand : ICommand
+    {
+        private Game1 myGame;
+        private SuperMarioGame.ElementClasses.Mario mario;
+
+        public MarioMoveLeftCommand(Game1 game)
+        {
+            myGame = game;
+            mario = myGame.mario;
+        }
+
+        public void Execute()
+        {
+
+            mario.position = new Vector2(mario.position.X - 1, mario.position.Y);
+        }
+    }
+}
---
+++ SuperMarioGame/SuperMarioGame/Commands/MarioMoverightCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace SuperMarioGame.Commands.Sprint3
+{
+    class MarioMoveRightCommand : ICommand
+    {
+        private Game1 myGame;
+        private SuperMarioGame.ElementClasses.Mario mario;
+
+        public MarioMoveRightCommand(Game1 game)
+        {
+            myGame = game;
+            mario = myGame.mario;
+        }
+
+        public void Execute()
+        {
+
+            mario.position = new Vector2(mario.position.X + 1, mario.position.Y);
+        }
+    }
+}
