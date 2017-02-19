7882fc7742f38539269262d1c39cedb869a31e0f
Chris Crain <chrisac309@gmail.com>
2017-02-19 16:39:09 -0500

    Another one

--- SuperMarioGame/SuperMarioGame/CollisionHandler/CollisionDetection.cs
+++ SuperMarioGame/SuperMarioGame/CollisionHandler/CollisionDetection.cs
@@ -50,12 +50,10 @@ namespace SuperMarioGame.CollisionHandler
                         if (firstRectangle.Top > secondRectangle.Top)
                         {
                             SIDE = 3;
-                            MarioBlockHandler.BlockHandler(mario,block,3);
                         }
                         else
                         {
                             SIDE = 1;
-                            MarioBlockHandler.BlockHandler(mario, block, 1);
                         }
                     }
                     else if (collideRectangle.Width < collideRectangle.Height)
@@ -63,15 +61,13 @@ namespace SuperMarioGame.CollisionHandler
                         if (firstRectangle.Left > secondRectangle.Left)
                         {
                             SIDE = 4;
-                            MarioBlockHandler.BlockHandler(mario, block, 4);
                         }
                         else
                         {
                             SIDE = 2;
-                            MarioBlockHandler.BlockHandler(mario, block, 2);
                         }
                     }
-                    MarioBlockHandler.Instance.BlockHandler(mario, block, SIDE);
+                    MarioBlockHandler.BlockHandler(mario, block, SIDE);
                 }
                 
             }
