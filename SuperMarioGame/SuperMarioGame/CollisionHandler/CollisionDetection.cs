6232528611a02bd463fd14f7e1daac9ea9ce3e9a
Nathan Pratt <pratt.251@osu.edu>
2017-02-19 12:12:54 -0500

    collision detection

--- SuperMarioGame/SuperMarioGame/CollisionHandler/CollisionDetection.cs
+++ SuperMarioGame/SuperMarioGame/CollisionHandler/CollisionDetection.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SuperMarioGame.ElementClasses;
 
 namespace SuperMarioGame.CollisionHandler
 {
     class CollisionDetection
     {
+        private Mario mario;
 
     }
 }
