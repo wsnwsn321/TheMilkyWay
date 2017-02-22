5b63df026798d03497ac678e796df61f261c3712
songnan wu <tcwsn321@outlook.com>
2017-02-22 17:58:41 -0500

    test case example

---
+++ SuperMarioGame/SuperMarioGame/TestCase/TestMarioBlockCollision.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using SuperMarioGame.ElementClasses.ElementInterfaces;
+using SuperMarioGame.ElementClasses;
+using Microsoft.Xna.Framework;
+using SuperMarioGame.ElementClasses.EnvironmentClass;
+using SuperMarioGame.CollisionHandler;
+
+namespace SuperMarioGame.TestCase
+{
+    class TestMarioBlockCollision
+    {
+        public void MarioBlockTopCollision()
+        {
+            bool isV = false;
+            Mario mario = new Mario(new Vector2(400, 400), Mario.MARIO_SMALL, false);
+            int blockposotion = 400-mario.state.marioSprite.desRectangle.Height;
+            IBlock block = new QuestionBlock(new Vector2(400, blockposotion));
+            MarioBlockHandler.BlockHandler(mario, block,3);
+            if (isV != block.isVisible)
+            {
+                //test case not pass
+            }
+            if(!(block is UsedBlock))
+            {
+                //test case not pass
+            }
+            
+         }
+
+        
+    }
+}
