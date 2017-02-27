3a8756eb841b84f72770b53df45fdcb69c0feeab
Nathan Pratt <pratt.251@osu.edu>
2017-02-27 14:00:31 -0500

    command stuff

--- SuperMarioGame/SuperMarioGame/Commands/MarioRightCommand.cs
+++ SuperMarioGame/SuperMarioGame/Commands/MarioRightCommand.cs
@@ -27,14 +27,6 @@ namespace SuperMarioGame.Commands
                 {
                     mario.position = new Vector2(mario.position.X + 3, mario.position.Y);
                 }                
-                if (mario.position.Y > 0)
-                {
-                    mario.position = new Vector2(mario.position.X, mario.position.Y - 3);
-                }
-                if (mario.position.Y < myGame.Window.ClientBounds.Height - mario.state.marioSprite.desRectangle.Height)
-                {
-                    mario.position = new Vector2(mario.position.X, mario.position.Y + 3);
-                }
             }
              
         }
