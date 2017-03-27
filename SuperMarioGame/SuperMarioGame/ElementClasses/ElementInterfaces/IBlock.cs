c07ca3f78ecedd9f7bfed2280f87a455dc96429b
Chris Crain <chrisac309@gmail.com>
2017-03-27 14:39:33 -0400

    BrickBlock and IBlock bump

--- SuperMarioGame/SuperMarioGame/ElementClasses/ElementInterfaces/IBlock.cs
+++ SuperMarioGame/SuperMarioGame/ElementClasses/ElementInterfaces/IBlock.cs
@@ -6,11 +6,15 @@ namespace SuperMarioGame.ElementClasses.ElementInterfaces
     public interface IBlock
     {
         Vector2 position { get; set; }
+        int bumpCount { get; set; }
         void Draw();
         void Update();
+        void Bump();
         ISprite blockSprite { get; set; }
         bool isVisible { get; set; }
         bool isBroken { get; set; }
+        bool isBumped { get; set; }
+
 
 
     }
