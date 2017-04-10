9c897b74dbd3aaad86e1fe3be438ecb3a33fc25c
Chris Crain <chrisac309@gmail.com>
2017-04-10 18:25:31 -0400

    BackgroundMusic

--- SuperMarioGame/SuperMarioGame/Sound/BackgroundMusic/BackGroundMusic.cs
+++ SuperMarioGame/SuperMarioGame/Sound/BackgroundMusic/BackGroundMusic.cs
@@ -1,15 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 using Microsoft.Xna.Framework.Content;
-using System.Collections.Generic;
-
 
 namespace SuperMarioGame.Sound.BackgroundMusic
 {
