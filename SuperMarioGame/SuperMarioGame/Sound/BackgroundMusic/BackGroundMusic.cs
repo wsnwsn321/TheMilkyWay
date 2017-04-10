9cf4767cfc17b710f02df81a76f34e4863492a81
George <yang.2766@osu.edu>
2017-04-10 16:37:24 -0400

    George

---
+++ SuperMarioGame/SuperMarioGame/Sound/BackgroundMusic/BackGroundMusic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using System.Collections.Generic;
+
+
+namespace SuperMarioGame.Sound.BackgroundMusic
+{
+    public class BackGroundMusic
+    {
+        public static BackGroundMusic instanse = new BackGroundMusic();
+        List<SoundEffect> bgm;
+        SoundEffectInstance var;
+
+        public BackGroundMusic()
+        {
+
+        }
+    }
+}
