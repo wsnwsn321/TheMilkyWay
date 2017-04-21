70ad81964a9e5cc5c01e08931fcd4ca5f6f6048f
Nathan Pratt <pratt.251@osu.edu>
2017-04-21 16:38:16 -0400

    window menu stuff

---
+++ Sprint6/Sprint6/Windows/IWindow.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xna.Framework;
+using Sprint6.Sprites;
+using Sprint6.Sprites.UFOSprite;
+
+namespace Sprint6
+{
+    public interface IWindow
+    {
+        Rectangle windowRect { get; set; }
+        void Display();
+        bool isVisible { get; set; }
+    }
+}
