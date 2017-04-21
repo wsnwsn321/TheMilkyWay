21b240d6917428cbfdbcd821a9edc6b76c222f52
Nathan Pratt <pratt.251@osu.edu>
2017-04-21 18:06:46 -0400

    sync

--- Sprint6/Sprint6/Windows/MenuWindow.cs
+++ Sprint6/Sprint6/Windows/MenuWindow.cs
@@ -30,17 +30,17 @@ namespace Sprint6.HUDElements
                 Color.White,
             };
             pixel.SetData<Color>(colorData);
-            windowRect = new Rectangle(25,GameConstants.ScreenHeight-175,255,155);
-            borderRect = new Rectangle(20,GameConstants.ScreenHeight-180,265,165);
-            menuItem1.X = windowRect.X + windowRect.Width / 2;
+            windowRect = new Rectangle(25,GameConstants.ScreenHeight-175,400,150);
+            borderRect = new Rectangle(20,GameConstants.ScreenHeight-180,410,160);
+            menuItem1.X = (windowRect.X + windowRect.Width / 2)+8;
             menuItem1.Y = windowRect.Y + 10 + windowRect.Height / 2;
         }
 
         public void Display()
         {
-            String inst = "How to Play:\nRepeatedly press space bar in\norder to keep the UFO in the sky,\n and try to avoid all obstacles" +
-                          "\nTry to collect all of the cows in the level\n using the beam ('b')" +
-                          "\nDo NOT collect toxic green cows\n instead blow them up with bombs ('n')\n";
+            String inst = "How to Play:\n-Repeatedly press space bar in order to keep\n the UFO in the sky, and try to avoid all obstacles\n" +
+                          "-Try to collect all of the cows using the beam ('b')\n" +
+                          "-Do NOT collect toxic green cows\n instead blow them up with bombs ('n')\n";
 
             Vector2 FontOrigin1 = mygame.font.MeasureString(inst) / GameConstants.Two;
             if (isVisible)
@@ -49,7 +49,7 @@ namespace Sprint6.HUDElements
                 mygame.spriteBatch.Draw(pixel, borderRect, Color.White);
                 mygame.spriteBatch.Draw(pixel, windowRect, Color.Gray);
                 mygame.spriteBatch.DrawString(mygame.font, inst, menuItem1, Color.White,
-                    0, FontOrigin1, 0.9f, SpriteEffects.None, 1f);
+                    0, FontOrigin1, 1f, SpriteEffects.None, 1f);
                 mygame.spriteBatch.End();
             }
         }
